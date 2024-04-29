ed71405226cbe429bb3b29433376330d27d28793
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-04-29 12:54:14 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
 
-const PostSchema = new Schema({
+const postSchema = new mongoose.Schema({
     title: String,
     summary: String,
     content: String,
     cover: String,
     author: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
-}, {
-    timestamps: true
-});
+}, { timestamps: true });
 
-const PostModel = model('Post', PostSchema);
-module.exports = PostModel;
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
